989144e38b52fcd958a39c58fc0b27e16467340a
hamster88 <hamster88@users.noreply.github.com>
2024-10-26 00:51:51 +0800

    feat: 设置持久化

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Threading;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using static System.Net.Mime.MediaTypeNames;
+using Microsoft.Win32.SafeHandles;
+using System.Data;
 
 namespace floating_clock
 {
@@ -23,7 +27,7 @@ namespace floating_clock
     /// 
     public partial class MainWindow : Window
     {
-        public DataModel data =  new DataModel();
+        public DataModel data = new DataModel();
         private DispatcherTimer timer = new DispatcherTimer();
 
         public MainWindow()
@@ -36,7 +40,7 @@ namespace floating_clock
             timer.Tick += Timer_Tick;
             timer.Start();
 
-            layout.DataContext = data;
+            this.DataContext = data;
 
         }
 
@@ -53,7 +57,7 @@ namespace floating_clock
             MenuItem menuItem = sender as MenuItem;
             if (menuItem != null)
             {
-                MessageBox.Show($"功能未实现： {menuItem.Header}");
+                MessageBox.Show($"功能未实现： {menuItem.Header}  ");
             }
         }
 
@@ -62,18 +66,22 @@ namespace floating_clock
             MenuItem menuItem = sender as MenuItem;
             if (menuItem != null)
             {
-                MessageBox.Show($"功能未实现： {menuItem.Header}");
+                MessageBox.Show($"功能未实现： {menuItem.Header}  ");
             }
         }
 
         private void MenuItem_Click_Exit(object sender, RoutedEventArgs e)
         {
+            //Properties.Settings.Default.Save();
+            data.SaveSetting();
             Close();
         }
 
         private void MenuItem_Click_Help(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"Floating Clock / v0.0.1");
+            var text = "Floating Clock / v0.0.1";
+            text += ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).FilePath;
+            MessageBox.Show(text);
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -87,23 +95,37 @@ namespace floating_clock
             // TODO: 记录窗口停靠位置
             var workingArea = SystemParameters.WorkArea;
             this.Left = workingArea.Right - this.Width;
-            this.Top = (workingArea.Bottom/2) - this.Height;
+            this.Top = (workingArea.Bottom / 2) - this.Height;
         }
     }
 
-    public static class Setting
+    /// <summary>
+    /// 用于用户设置的状态管理和持久化（未完成）
+    /// </summary>
+    public class Setting
     {
-        // TODO: 
-        public static Dictionary<string, string> visibility_state = new Dictionary<string, string>();
-        
-        public static void Load()
+        public string Topmost { get; set; } = "True";
+        public string VisibilityClock { get; set; } = "Visible";
+        public string VisibilityCPU { get; set; } = "Visible";
+        public string VisibilityRAM { get; set; } = "Visible";
+        public string VisibilityUpload { get; set; } = "Visible";
+        public string VisibilityDownload { get; set; } = "Visible";
+
+        public Setting()
+        {
+            Load();
+        }
+
+        public void Load()
         {
             var _ = ConfigurationManager.AppSettings;
-            visibility_state["clock"] = _["clock"] ?? "Collapsed";
-            visibility_state["CPU"] = _["CPU"] ?? "Visible";
-            visibility_state["RAM"] = _["RAM"] ?? "Visible";
-            visibility_state["upload"] = _["upload"] ?? "Visible";
-            visibility_state["download"] = _["download"] ?? "Visible";
+
+            VisibilityClock = _["clock"] ?? "Collapsed";
+            //visibility_state["clock"] = _["clock"] ?? "Collapsed";
+            //visibility_state["CPU"] = _["CPU"] ?? "Visible";
+            //visibility_state["RAM"] = _["RAM"] ?? "Visible";
+            //visibility_state["upload"] = _["upload"] ?? "Visible";
+            //visibility_state["download"] = _["download"] ?? "Visible";
 
 
         }
@@ -113,13 +135,16 @@ namespace floating_clock
         }
     }
 
-
+    /// <summary>
+    /// 主数据模型
+    /// </summary>
     public class DataModel : INotifyPropertyChanged
     {
 
         SystemMonitor systemMonitor = new SystemMonitor();
         long last_up = 0;
         long last_down = 0;
+        DateTime last_save_setting_at = DateTime.MinValue;
 
         public string Time { get; set; } = "";
         public string CPU { get; set; } = "";
@@ -127,23 +152,143 @@ namespace floating_clock
         public string Upload { get; set; } = "";
         public string Download { get; set; } = "";
 
+        //public ObservableProperty<bool> Topmost { get; set; } = new();
+        //public ObservableProperty<string> Topmost { get; set; } = new("True");
+
+        public string VisibilityClock { get; set; } = "Visible";
+        public string VisibilityCPU { get; set; } = "Visible";
+        public string VisibilityRAM { get; set; } = "Visible";
+        public string VisibilityUpload { get; set; } = "Visible";
+        public string VisibilityDownload { get; set; } = "Visible";
+
+
+
+        bool topmost = true;
+        bool showClock = true;
+        bool showCPU = true;
+        bool showRAM = true;
+        bool showUpload = true;
+        bool showDownload = true;
+
+        public bool Topmost
+        {
+            get { return topmost; }
+            set
+            {
+                if (!Equals(this.topmost, value))
+                {
+                    this.topmost = value;
+                    ChangeSetting();
+                    OnPropertyChanged(nameof(Topmost));
+                }
+            }
+        }
+
+        public bool ShowClock
+        {
+            get { return showClock; }
+            set
+            {
+                if (!Equals(this.showClock, value))
+                {
+                    this.showClock = value;
+                    VisibilityClock = value ? "Visible" : "Collapsed";
+                    ChangeSetting();
+                    OnPropertyChanged(nameof(VisibilityClock));
+                    OnPropertyChanged(nameof(ShowClock));
+                }
+            }
+        }
+
+        public bool ShowCPU
+        {
+            get { return showCPU; }
+            set
+            {
+                if (!Equals(this.showCPU, value))
+                {
+                    this.showCPU = value;
+                    VisibilityCPU = value ? "Visible" : "Collapsed";
+                    ChangeSetting();
+                    OnPropertyChanged(nameof(VisibilityCPU));
+                    OnPropertyChanged(nameof(ShowCPU));
+                }
+            }
+        }
+
+        public bool ShowRAM
+        {
+            get { return showRAM; }
+            set
+            {
+                if (!Equals(this.showRAM, value))
+                {
+                    this.showRAM = value;
+                    VisibilityRAM = value ? "Visible" : "Collapsed";
+                    ChangeSetting();
+                    OnPropertyChanged(nameof(VisibilityRAM));
+                    OnPropertyChanged(nameof(ShowRAM));
+                }
+            }
+        }
+
+        public bool ShowUpload
+        {
+            get { return showUpload; }
+            set
+            {
+                if (!Equals(this.showUpload, value))
+                {
+                    this.showUpload = value;
+                    VisibilityUpload = value ? "Visible" : "Collapsed";
+                    ChangeSetting();
+                    OnPropertyChanged(nameof(VisibilityUpload));
+                    OnPropertyChanged(nameof(ShowUpload));
+                }
+            }
+        }
+
+        public bool ShowDownload
+        {
+            get { return showDownload; }
+            set
+            {
+                if (!Equals(this.showDownload, value))
+                {
+                    this.showDownload = value;
+                    VisibilityDownload = value ? "Visible" : "Collapsed";
+                    ChangeSetting();
+                    OnPropertyChanged(nameof(VisibilityDownload));
+                    OnPropertyChanged(nameof(ShowDownload));
+                }
+            }
+        }
+
 
         public DataModel()
         {
             Update();
+            LoadSetting();
+            // ChangeSetting();
+
         }
 
         public DataModel Update()
         {
-            Time = DateTime.Now.ToString("HH : mm : ss");
-
+           var now = DateTime.Now;
+            Time = now.ToString("HH : mm : ss");
 
             var cpu = systemMonitor.GetCpuUsage();
             var ram = systemMonitor.GetAvailableRam();
+
+            CPU = $"{cpu:F2}%";
+            RAM = Util.ConvertBytesToReadableSize(ram);
+
+            // 计算网速
             long up = 0;
             long down = 0;
 
-            // 计算网速
+            // 获取网卡列表，和流量计数
             var net = systemMonitor.GetNetworkStatistics();
             foreach (var item in net)
             {
@@ -151,33 +296,141 @@ namespace floating_clock
                 down += item.BytesReceived;
             }
 
-
-            CPU= $"{cpu:F2}%";
-            RAM = Util.ConvertBytesToReadableSize(ram);
-
             if (last_down + last_up > 0) // 第一次获取流量，跳过计算
             {
                 Upload = Util.ConvertBytesToReadableSize((up - last_up) / 1000.0, "kmg") + "/s";
                 Download = Util.ConvertBytesToReadableSize((down - last_down) / 1000.0, "kmg") + "/s";
+            }
+            else
+            {
+                Upload = "0/ks";
+                Download = "0/ks";
             }
 
             last_up = up;
             last_down = down;
 
 
-
+            // 更新视图
             OnPropertyChanged(nameof(Time));
             OnPropertyChanged(nameof(CPU));
             OnPropertyChanged(nameof(RAM));
             OnPropertyChanged(nameof(Upload));
             OnPropertyChanged(nameof(Download));
 
+
+            if((now - last_save_setting_at).TotalSeconds > 5)
+            {
+                last_save_setting_at = now;
+                SaveSetting();
+            }
+
+
             return this;
+        }
+
+
+        public void LoadSetting()
+        {
+            var _ = ConfigurationManager.AppSettings;
+            Topmost = bool.Parse(_["Topmost"] ?? "true");
+            ShowClock = bool.Parse(_["ShowClock"] ?? "true");
+            ShowCPU = bool.Parse(_["ShowCPU"] ?? "true");
+            ShowRAM = bool.Parse(_["ShowRAM"] ?? "true");
+            ShowUpload = bool.Parse(_["ShowUpload"] ?? "true");
+            ShowDownload = bool.Parse(_["ShowDownload"] ?? "true");
+
+            //Properties.Settings.Default.SaveSetting = true;
+        }
+
+
+
+        public void ChangeSetting()
+        {
+            // 弃用
+        }
+
+
+
+        public void SaveSetting()
+        {
+            
+
+            
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = config.AppSettings.Settings;
+                
+            if(settings["Topmost"] == null)
+                settings.Add("Topmost", Topmost.ToString());
+            else
+                settings["Topmost"].Value = Topmost.ToString();
+
+            if (settings["ShowClock"] == null)
+                settings.Add("ShowClock", showClock.ToString());
+            else
+                settings["ShowClock"].Value = showClock.ToString();
+
+            if (settings["ShowCPU"] == null)
+                settings.Add("ShowCPU", showCPU.ToString());
+            else
+                settings["ShowCPU"].Value = showCPU.ToString();
+
+            if (settings["ShowRAM"] == null)
+                settings.Add("ShowRAM", showRAM.ToString());
+            else
+                settings["ShowRAM"].Value = showRAM.ToString();
+
+            if (settings["ShowUpload"] == null)
+                settings.Add("ShowUpload", showUpload.ToString());
+            else
+                settings["ShowUpload"].Value = showUpload.ToString();
+
+            if (settings["ShowDownload"] == null)
+                settings.Add("ShowDownload", showDownload.ToString());
+            else
+                settings["ShowDownload"].Value = showDownload.ToString();
+            config.Save(ConfigurationSaveMode.Modified);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class ObservableProperty<T> : INotifyPropertyChanged
+    {
+        private T value;
+
+        public T Value
+        {
+            get { return value; }
+            set
+            {
+                if (!Equals(this.value, value))
+                {
+                    this.value = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableProperty()
+        {
+
+        }
+
+        public ObservableProperty(T value)
+        {
+            Value = value;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
